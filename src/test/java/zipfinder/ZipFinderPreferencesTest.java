aff74b1a641e9c2e5b71fbb56226aef9a5898a88
Björn Ekryd <bjornatekryd.se>
2023-09-07 12:51:15 +0200

    Update code to modern frameworks

--- src/test/java/zipfinder/ZipFinderPreferencesTest.java
+++ src/test/java/zipfinder/ZipFinderPreferencesTest.java
@@ -1,239 +1,184 @@
 package zipfinder;
 
-import java.util.LinkedList;
-import java.util.prefs.Preferences;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.LinkedList;import java.util.prefs.Preferences;
 import junit.framework.TestCase;
 import zipfinder.testutil.ReflectionHelper;
-import cheesymock.Cheesy;
-import cheesymock.Recorder;
 
 @SuppressWarnings("unused")
 public class ZipFinderPreferencesTest extends TestCase {
 	private ZipFinderPreferences zipFinderPreferences = new ZipFinderPreferences();
-	private Preferences preferencesMock;
+	private Preferences preferencesMock = mock(Preferences.class);
 	private String store;
-	private Recorder recorder;
 
 	public final void testAddDirectoryEmpty() throws Exception {
-		// Setup
-		store = "";
-		// expect
-		recorder.expect(preferencesMock).get("Directories", "");
-		recorder.expect(preferencesMock).put("Directories", "pelle");
-		recorder.expect(preferencesMock).sync();
-		// replay
+			when(preferencesMock.get("Directories", "")).thenReturn("");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
 		zipFinderPreferences.addDirectory("pelle");
-		recorder.check();
-		// assert
+
+		verify(preferencesMock).get("Directories", "");
+		verify(preferencesMock).put("Directories", "pelle");
+		verify(preferencesMock).sync();
 	}
 
 	public final void testAddDirectoryExistingEntry() throws Exception {
-		// Setup
-		store = "anna|stina|olle";
-		// expect
-		recorder.expect(preferencesMock).get("Directories", "");
-		recorder.expect(preferencesMock).put("Directories", "stina|anna|olle");
-		recorder.expect(preferencesMock).sync();
-		// replay
+			when(preferencesMock.get("Directories", "")).thenReturn("anna|stina|olle");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
 		zipFinderPreferences.addDirectory("stina");
-		recorder.check();
-		// assert
+		verify(preferencesMock).get("Directories", "");
+		verify(preferencesMock).put("Directories", "stina|anna|olle");
+		verify(preferencesMock).sync();
 	}
 
 	public final void testAddDirectoryOneEntry() throws Exception {
-		// Setup
-		store = "anna";
-		// expect
-		recorder.expect(preferencesMock).get("Directories", "");
-		recorder.expect(preferencesMock).put("Directories", "pelle|anna");
-		recorder.expect(preferencesMock).sync();
-		// replay
+			when(preferencesMock.get("Directories", "")).thenReturn("anna");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
 		zipFinderPreferences.addDirectory("pelle");
-		recorder.check();
-		// assert
+		verify(preferencesMock).get("Directories", "");
+		verify(preferencesMock).put("Directories", "pelle|anna");
+		verify(preferencesMock).sync();
 	}
 
 	public final void testAddDirectoryThreeEntry() throws Exception {
-		// Setup
-		store = "anna|C:\\Documents and Settings\\Bjorn\\.saverbeans|olle";
-		// expect
-		recorder.expect(preferencesMock).get("Directories", "");
-		recorder.expect(preferencesMock).put("Directories",
-				"C:\\Documents and Settings|anna|C:\\Documents and Settings\\Bjorn\\.saverbeans|olle");
-		recorder.expect(preferencesMock).sync();
-		// replay
+			when(preferencesMock.get("Directories", "")).thenReturn("anna|C:\\Documents and Settings\\Bjorn\\.saverbeans|olle");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
 		zipFinderPreferences.addDirectory("C:\\Documents and Settings");
-		recorder.check();
-		// assert
+		verify(preferencesMock).get("Directories", "");
+		verify(preferencesMock).put("Directories",
+				"C:\\Documents and Settings|anna|C:\\Documents and Settings\\Bjorn\\.saverbeans|olle");
+		verify(preferencesMock).sync();
 	}
 
 	public final void testAddDirectoryTooManyEntryEntry() throws Exception {
-		// Setup
-		store = "1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30";
-		// expect
-		recorder.expect(preferencesMock).get("Directories", "");
-		recorder.expect(preferencesMock).put("Directories", "stina|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19");
-		recorder.expect(preferencesMock).sync();
-		// replay
+			when(preferencesMock.get("Directories", "")).thenReturn("1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
 		zipFinderPreferences.addDirectory("stina");
-		recorder.check();
-		// assert
+		verify(preferencesMock).get("Directories", "");
+		verify(preferencesMock).put("Directories",
+				"stina|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19");
+		verify(preferencesMock).sync();
 	}
 
 	public final void testAddStringToFindEmpty() throws Exception {
-		// Setup
-		store = "";
-		// expect
-		recorder.expect(preferencesMock).get("StringsToFind", "");
-		recorder.expect(preferencesMock).put("StringsToFind", "pelle");
-		recorder.expect(preferencesMock).sync();
-		// replay
+			when(preferencesMock.get("StringsToFind", "")).thenReturn("");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
 		zipFinderPreferences.addStringToFind("pelle");
-		recorder.check();
-		// assert
+
+		verify(preferencesMock).get("StringsToFind", "");
+		verify(preferencesMock).put("StringsToFind", "pelle");
+		verify(preferencesMock).sync();
 	}
 
 	public final void testAddStringToFindExistingEntry() throws Exception {
-		// Setup
-		store = "anna|stina|olle";
-		// expect
-		recorder.expect(preferencesMock).get("StringsToFind", "");
-		recorder.expect(preferencesMock).put("StringsToFind", "stina|anna|olle");
-		recorder.expect(preferencesMock).sync();
-		// replay
+			when(preferencesMock.get("StringsToFind", "")).thenReturn("anna|stina|olle");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
 		zipFinderPreferences.addStringToFind("stina");
-		recorder.check();
-		// assert
+		verify(preferencesMock).get("StringsToFind", "");
+		verify(preferencesMock).put("StringsToFind", "stina|anna|olle");
+		verify(preferencesMock).sync();
 	}
 
 	public final void testAddStringToFindOneEntry() throws Exception {
-		// Setup
-		store = "anna";
-		// expect
-		recorder.expect(preferencesMock).get("StringsToFind", "");
-		recorder.expect(preferencesMock).put("StringsToFind", "pelle|anna");
-		recorder.expect(preferencesMock).sync();
-		// replay
+			when(preferencesMock.get("StringsToFind", "")).thenReturn("pelle|anna");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
 		zipFinderPreferences.addStringToFind("pelle");
-		recorder.check();
-		// assert
+		verify(preferencesMock).get("StringsToFind", "");
+		verify(preferencesMock).put("StringsToFind", "pelle|anna");
+		verify(preferencesMock).sync();
 	}
 
 	public final void testAddStringToFindThreeEntry() throws Exception {
-		// Setup
-		store = "anna|stina|olle";
-		// expect
-		recorder.expect(preferencesMock).get("StringsToFind", "");
-		recorder.expect(preferencesMock).put("StringsToFind", "pelle|anna|stina|olle");
-		recorder.expect(preferencesMock).sync();
-		// replay
-		zipFinderPreferences.addStringToFind("pelle");
-		recorder.check();
-		// assert
+			when(preferencesMock.get("StringsToFind", "")).thenReturn("anna|C:\\Documents and Settings\\Bjorn\\.saverbeans|olle");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
+		zipFinderPreferences.addStringToFind("C:\\Documents and Settings");
+		verify(preferencesMock).get("StringsToFind", "");
+		verify(preferencesMock).put("StringsToFind",
+				"C:\\Documents and Settings|anna|C:\\Documents and Settings\\Bjorn\\.saverbeans|olle");
+		verify(preferencesMock).sync();
 	}
 
 	public final void testAddStringToFindTooManyEntryEntry() throws Exception {
-		// Setup
-		store = "1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30";
-		// expect
-		recorder.expect(preferencesMock).get("StringsToFind", "");
-		recorder.expect(preferencesMock).put("StringsToFind", "stina|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19");
-		recorder.expect(preferencesMock).sync();
-		// replay
+			when(preferencesMock.get("StringsToFind", "")).thenReturn("1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
 		zipFinderPreferences.addStringToFind("stina");
-		recorder.check();
-		// assert
+		verify(preferencesMock).get("StringsToFind", "");
+		verify(preferencesMock).put("StringsToFind",
+				"stina|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19");
+		verify(preferencesMock).sync();
 	}
 
-	public final void testGetRecentDirectoriesEmpty() {
-		// Setup
-		store = "";
-		// expect
-		recorder.expect(preferencesMock).get("Directories", "");
-		// replay
-		final LinkedList<String> recentDirectories = zipFinderPreferences.getRecentDirectories();
-		recorder.check();
-		// assert
+	public final void testGetRecentDirectoriesEmpty()throws IllegalAccessException {
+			when(preferencesMock.get("Directories", "")).thenReturn("");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
+		LinkedList<String> recentDirectories = zipFinderPreferences.getRecentDirectories();
 		assertEquals(0, recentDirectories.size());
 	}
 
-	public final void testGetRecentDirectoriesManyEntry() {
-		// Setup
-		store = "pelle|C:\\Documents and Settings\\Bjorn\\.saverbeans|stina";
-		// expect
-		recorder.expect(preferencesMock).get("Directories", "");
-		// replay
-		final LinkedList<String> recentDirectories = zipFinderPreferences.getRecentDirectories();
-		recorder.check();
-		// assert
+	public final void testGetRecentDirectoriesManyEntry()throws IllegalAccessException {
+			when(preferencesMock.get("Directories", "")).thenReturn("pelle|C:\\Documents and Settings\\Bjorn\\.saverbeans|stina");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
+		LinkedList<String> recentDirectories = zipFinderPreferences.getRecentDirectories();
+
+		verify(preferencesMock).get("Directories", "");
 		assertEquals(3, recentDirectories.size());
 		assertEquals("pelle", recentDirectories.get(0));
 		assertEquals("C:\\Documents and Settings\\Bjorn\\.saverbeans", recentDirectories.get(1));
 		assertEquals("stina", recentDirectories.get(2));
 	}
 
-	public final void testGetRecentDirectoriesOneEntry() {
-		// Setup
-		store = "pelle";
-		// expect
-		recorder.expect(preferencesMock).get("Directories", "");
-		// replay
-		final LinkedList<String> recentDirectories = zipFinderPreferences.getRecentDirectories();
-		recorder.check();
-		// assert
+	public final void testGetRecentDirectoriesOneEntry()throws IllegalAccessException {
+			when(preferencesMock.get("Directories", "")).thenReturn("pelle");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
+		LinkedList<String> recentDirectories = zipFinderPreferences.getRecentDirectories();
+
+		verify(preferencesMock).get("Directories", "");
 		assertEquals(1, recentDirectories.size());
 		assertEquals("pelle", recentDirectories.get(0));
 	}
 
-	public final void testGetRecentStringsToFindEmpty() {
-		// Setup
-		store = "";
-		// expect
-		recorder.expect(preferencesMock).get("StringsToFind", "");
-		// replay
-		final LinkedList<String> recentDirectories = zipFinderPreferences.getRecentStringsToFind();
-		recorder.check();
-		// assert
+	public final void testGetRecentStringsToFindEmpty()throws IllegalAccessException {
+			when(preferencesMock.get("StringsToFind", "")).thenReturn("");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
+		LinkedList<String> recentDirectories = zipFinderPreferences.getRecentStringsToFind();
+		verify(preferencesMock).get("StringsToFind", "");
 		assertEquals(0, recentDirectories.size());
 	}
 
-	public final void testGetRecentStringsToFindManyEntry() {
-		// Setup
-		store = "pelle|anna|stina";
-		// expect
-		recorder.expect(preferencesMock).get("StringsToFind", "");
-		// replay
-		final LinkedList<String> recentDirectories = zipFinderPreferences.getRecentStringsToFind();
-		recorder.check();
-		// assert
+	public final void testGetRecentStringsToFindManyEntry()throws IllegalAccessException {
+			when(preferencesMock.get("StringsToFind", "")).thenReturn("pelle|anna|stina");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
+		LinkedList<String> recentDirectories = zipFinderPreferences.getRecentStringsToFind();
+		verify(preferencesMock).get("StringsToFind", "");
 		assertEquals(3, recentDirectories.size());
 		assertEquals("pelle", recentDirectories.get(0));
 		assertEquals("anna", recentDirectories.get(1));
 		assertEquals("stina", recentDirectories.get(2));
 	}
 
-	public final void testGetRecentStringsToFindOneEntry() {
-		// Setup
-		store = "pelle";
-		// expect
-		recorder.expect(preferencesMock).get("StringsToFind", "");
-		// replay
-		final LinkedList<String> recentDirectories = zipFinderPreferences.getRecentStringsToFind();
-		recorder.check();
-		// assert
+	public final void testGetRecentStringsToFindOneEntry()throws IllegalAccessException {
+			when(preferencesMock.get("StringsToFind", "")).thenReturn("pelle");
+		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
+
+		LinkedList<String> recentDirectories = zipFinderPreferences.getRecentStringsToFind();
+		verify(preferencesMock).get("StringsToFind", "");
 		assertEquals(1, recentDirectories.size());
 		assertEquals("pelle", recentDirectories.get(0));
 	}
 
-	@Override
-	protected void setUp() throws Exception {
-		preferencesMock = Cheesy.mock(Preferences.class, new Object() {
-			public String get(final String key, final String def) {
-				return store;
-			}
-		});
-		recorder = new Recorder();
-		new ReflectionHelper(zipFinderPreferences).setField(preferencesMock);
-	}
 }
